d274fc8f0c36cac1483dac4d7c50d861b5c65f0d
Robin Uppman <[robin.uppman@nexergroup.com]>
2026-03-16 15:40:01 +0100

    intital view before dropping

---
+++ dbo.ViewFactFteAft2025.sql
@@ -0,0 +1,17 @@
+USE [MKBBIDW]
+GO
+
+/****** Object:  View [dbo].[ViewFactFteAft2025]    Script Date: 2026-03-16 15:38:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[ViewFactFteAft2025]
+as
+SELECT *
+FROM [MKBBIDW].[fact].[FactFTE]
+GO
+
+
